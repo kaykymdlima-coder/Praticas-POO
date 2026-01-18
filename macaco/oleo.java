6a17e460aae331cabc247c1c52c67f5f10ef5cff
Samia8120-coder <medusa8120@gmail.com>
2026-01-18 19:35:22 -0300

    Delete macaco directory

--- macaco/oleo.java
+++
@@ -1,8 +0,0 @@
-public class Oleo extends Produto{
-    public int litros;
-
-    public Oleo(double preco, String nome, int quantia, int litros){
-        super(preco, nome, quantia);
-        this.litros = litros;
-    }
-}
